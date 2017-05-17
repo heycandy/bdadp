f9eac9248d59d0c2556647d5cb96cb36b6259933
baiyu <baiyu@chinasofti.com>
2017-05-17 16:08:22 +0800

    feat: 节假日不调度(for mysql)

---
+++ bdadp-misc/src/main/sql/data/ark_bdadp_new_holiday.sql
@@ -0,0 +1,268 @@
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
+--
+-- Host: host6    Database: ark_bdadp_new
+-- ------------------------------------------------------
+-- Server version	5.5.47-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `holiday`
+--
+
+LOCK TABLES `holiday` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+insert into holiday (holiday_id, holiday_value)
+values (3, '20170107');
+insert into holiday (holiday_id, holiday_value)
+values (1, '20170101');
+insert into holiday (holiday_id, holiday_value)
+values (2, '20170102');
+insert into holiday (holiday_id, holiday_value)
+values (4, '20170108');
+insert into holiday (holiday_id, holiday_value)
+values (5, '20170114');
+insert into holiday (holiday_id, holiday_value)
+values (6, '20170115');
+insert into holiday (holiday_id, holiday_value)
+values (7, '20170121');
+insert into holiday (holiday_id, holiday_value)
+values (8, '20170127');
+insert into holiday (holiday_id, holiday_value)
+values (9, '20170128');
+insert into holiday (holiday_id, holiday_value)
+values (10, '20170129');
+insert into holiday (holiday_id, holiday_value)
+values (11, '20170130');
+insert into holiday (holiday_id, holiday_value)
+values (12, '20170131');
+insert into holiday (holiday_id, holiday_value)
+values (13, '20170201');
+insert into holiday (holiday_id, holiday_value)
+values (14, '20170202');
+insert into holiday (holiday_id, holiday_value)
+values (15, '20170205');
+insert into holiday (holiday_id, holiday_value)
+values (16, '20170211');
+insert into holiday (holiday_id, holiday_value)
+values (17, '20170212');
+insert into holiday (holiday_id, holiday_value)
+values (18, '20170218');
+insert into holiday (holiday_id, holiday_value)
+values (19, '20170219');
+insert into holiday (holiday_id, holiday_value)
+values (20, '20170225');
+insert into holiday (holiday_id, holiday_value)
+values (21, '20170226');
+insert into holiday (holiday_id, holiday_value)
+values (22, '20170304');
+insert into holiday (holiday_id, holiday_value)
+values (23, '20170305');
+insert into holiday (holiday_id, holiday_value)
+values (24, '20170311');
+insert into holiday (holiday_id, holiday_value)
+values (25, '20170312');
+insert into holiday (holiday_id, holiday_value)
+values (26, '20170318');
+insert into holiday (holiday_id, holiday_value)
+values (27, '20170319');
+insert into holiday (holiday_id, holiday_value)
+values (28, '20170325');
+insert into holiday (holiday_id, holiday_value)
+values (29, '20170326');
+insert into holiday (holiday_id, holiday_value)
+values (30, '20170402');
+insert into holiday (holiday_id, holiday_value)
+values (31, '20170403');
+insert into holiday (holiday_id, holiday_value)
+values (32, '20170404');
+insert into holiday (holiday_id, holiday_value)
+values (33, '20170408');
+insert into holiday (holiday_id, holiday_value)
+values (34, '20170409');
+insert into holiday (holiday_id, holiday_value)
+values (35, '20170415');
+insert into holiday (holiday_id, holiday_value)
+values (36, '20170416');
+insert into holiday (holiday_id, holiday_value)
+values (37, '20170422');
+insert into holiday (holiday_id, holiday_value)
+values (38, '20170423');
+insert into holiday (holiday_id, holiday_value)
+values (39, '20170429');
+insert into holiday (holiday_id, holiday_value)
+values (40, '20170430');
+insert into holiday (holiday_id, holiday_value)
+values (41, '20170501');
+insert into holiday (holiday_id, holiday_value)
+values (42, '20170506');
+insert into holiday (holiday_id, holiday_value)
+values (43, '20170507');
+insert into holiday (holiday_id, holiday_value)
+values (44, '20170513');
+insert into holiday (holiday_id, holiday_value)
+values (45, '20170514');
+insert into holiday (holiday_id, holiday_value)
+values (46, '20170520');
+insert into holiday (holiday_id, holiday_value)
+values (47, '20170521');
+insert into holiday (holiday_id, holiday_value)
+values (48, '20170528');
+insert into holiday (holiday_id, holiday_value)
+values (49, '20170529');
+insert into holiday (holiday_id, holiday_value)
+values (50, '20170530');
+insert into holiday (holiday_id, holiday_value)
+values (51, '20170603');
+insert into holiday (holiday_id, holiday_value)
+values (52, '20170604');
+insert into holiday (holiday_id, holiday_value)
+values (53, '20170610');
+insert into holiday (holiday_id, holiday_value)
+values (54, '20170611');
+insert into holiday (holiday_id, holiday_value)
+values (55, '20170617');
+insert into holiday (holiday_id, holiday_value)
+values (56, '20170618');
+insert into holiday (holiday_id, holiday_value)
+values (57, '20170624');
+insert into holiday (holiday_id, holiday_value)
+values (58, '20170625');
+insert into holiday (holiday_id, holiday_value)
+values (59, '20170701');
+insert into holiday (holiday_id, holiday_value)
+values (60, '20170702');
+insert into holiday (holiday_id, holiday_value)
+values (61, '20170708');
+insert into holiday (holiday_id, holiday_value)
+values (62, '20170709');
+insert into holiday (holiday_id, holiday_value)
+values (63, '20170715');
+insert into holiday (holiday_id, holiday_value)
+values (64, '20170716');
+insert into holiday (holiday_id, holiday_value)
+values (65, '20170722');
+insert into holiday (holiday_id, holiday_value)
+values (66, '20170723');
+insert into holiday (holiday_id, holiday_value)
+values (67, '20170729');
+insert into holiday (holiday_id, holiday_value)
+values (68, '20170730');
+insert into holiday (holiday_id, holiday_value)
+values (69, '20170805');
+insert into holiday (holiday_id, holiday_value)
+values (70, '20170806');
+insert into holiday (holiday_id, holiday_value)
+values (71, '20170812');
+insert into holiday (holiday_id, holiday_value)
+values (72, '20170813');
+insert into holiday (holiday_id, holiday_value)
+values (73, '20170819');
+insert into holiday (holiday_id, holiday_value)
+values (74, '20170820');
+insert into holiday (holiday_id, holiday_value)
+values (75, '20170826');
+insert into holiday (holiday_id, holiday_value)
+values (76, '20170827');
+insert into holiday (holiday_id, holiday_value)
+values (77, '20170902');
+insert into holiday (holiday_id, holiday_value)
+values (78, '20170903');
+insert into holiday (holiday_id, holiday_value)
+values (79, '20170909');
+insert into holiday (holiday_id, holiday_value)
+values (80, '20170910');
+insert into holiday (holiday_id, holiday_value)
+values (81, '20170916');
+insert into holiday (holiday_id, holiday_value)
+values (82, '20170917');
+insert into holiday (holiday_id, holiday_value)
+values (83, '20170923');
+insert into holiday (holiday_id, holiday_value)
+values (84, '20170924');
+insert into holiday (holiday_id, holiday_value)
+values (85, '20171001');
+insert into holiday (holiday_id, holiday_value)
+values (86, '20171002');
+insert into holiday (holiday_id, holiday_value)
+values (87, '20171003');
+insert into holiday (holiday_id, holiday_value)
+values (88, '20171004');
+insert into holiday (holiday_id, holiday_value)
+values (89, '20171005');
+insert into holiday (holiday_id, holiday_value)
+values (90, '20171006');
+insert into holiday (holiday_id, holiday_value)
+values (91, '20171007');
+insert into holiday (holiday_id, holiday_value)
+values (92, '20171008');
+insert into holiday (holiday_id, holiday_value)
+values (93, '20171014');
+insert into holiday (holiday_id, holiday_value)
+values (94, '20171015');
+insert into holiday (holiday_id, holiday_value)
+values (95, '20171021');
+insert into holiday (holiday_id, holiday_value)
+values (96, '20171022');
+insert into holiday (holiday_id, holiday_value)
+values (97, '20171028');
+insert into holiday (holiday_id, holiday_value)
+values (98, '20171029');
+insert into holiday (holiday_id, holiday_value)
+values (99, '20171104');
+insert into holiday (holiday_id, holiday_value)
+values (100, '20171105');
+insert into holiday (holiday_id, holiday_value)
+values (101, '20171111');
+insert into holiday (holiday_id, holiday_value)
+values (102, '20171112');
+insert into holiday (holiday_id, holiday_value)
+values (103, '20171118');
+insert into holiday (holiday_id, holiday_value)
+values (104, '20171119');
+insert into holiday (holiday_id, holiday_value)
+values (105, '20171125');
+insert into holiday (holiday_id, holiday_value)
+values (106, '20171126');
+insert into holiday (holiday_id, holiday_value)
+values (107, '20171202');
+insert into holiday (holiday_id, holiday_value)
+values (108, '20171203');
+insert into holiday (holiday_id, holiday_value)
+values (109, '20171209');
+insert into holiday (holiday_id, holiday_value)
+values (110, '20171210');
+insert into holiday (holiday_id, holiday_value)
+values (111, '20171216');
+insert into holiday (holiday_id, holiday_value)
+values (112, '20171217');
+insert into holiday (holiday_id, holiday_value)
+values (113, '20171223');
+insert into holiday (holiday_id, holiday_value)
+values (114, '20171224');
+insert into holiday (holiday_id, holiday_value)
+values (115, '20171230');
+insert into holiday (holiday_id, holiday_value)
+values (116, '20171231');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-02-20 13:38:26
