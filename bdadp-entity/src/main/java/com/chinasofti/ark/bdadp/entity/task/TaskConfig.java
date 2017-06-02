5920d64b7aecb3f5751700881424e8e0909a7c5a
wangweiwei <672684719@qq.com>
2017-06-02 17:41:36 +0800

    del:去掉加密

--- bdadp-entity/src/main/java/com/chinasofti/ark/bdadp/entity/task/TaskConfig.java
+++ bdadp-entity/src/main/java/com/chinasofti/ark/bdadp/entity/task/TaskConfig.java
@@ -1,7 +1,7 @@
 package com.chinasofti.ark.bdadp.entity.task;
 
 import com.chinasofti.ark.bdadp.entity.components.ComponentConfig;
-import com.chinasofti.ark.bdadp.util.common.DESUtil;
+//import com.chinasofti.ark.bdadp.util.common.DESUtil;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -67,22 +67,30 @@ public class TaskConfig implements java.io.Serializable {
         this.paramId = paramId;
     }
 
-    public String getParamValue() {
-        if (null != paramValue && !"".equals(paramValue)) {
-            return DESUtil.getDecryptString(paramValue);
-        } else {
-            return "";
-        }
+//    public String getParamValue() {
+//        if (null != paramValue && !"".equals(paramValue)) {
+//            return DESUtil.getDecryptString(paramValue);
+//        } else {
+//            return "";
+//        }
+//
+//    }
+//
+//    public void setParamValue(String paramValue) {
+//
+//        if (null != paramValue && !"".equals(paramValue)) {
+//            this.paramValue = DESUtil.getEncryptString(paramValue);
+//        } else {
+//            this.paramValue = "";
+//        }
+//    }
 
+    public String getParamValue() {
+        return paramValue;
     }
 
     public void setParamValue(String paramValue) {
-
-        if (null != paramValue && !"".equals(paramValue)) {
-            this.paramValue = DESUtil.getEncryptString(paramValue);
-        } else {
-            this.paramValue = "";
-        }
+        this.paramValue = paramValue;
     }
 
     public Date getCreateTime() {
--- bdadp-util/src/main/java/com/chinasofti/ark/bdadp/util/common/DecryptPropertyPlaceholderConfigurer.java
+++ bdadp-util/src/main/java/com/chinasofti/ark/bdadp/util/common/DecryptPropertyPlaceholderConfigurer.java
@@ -1,52 +1,52 @@
-package com.chinasofti.ark.bdadp.util.common;
-
-import java.util.Properties;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
-
-/**
- * Created by water on 2017.5.11.
- */
-public class DecryptPropertyPlaceholderConfigurer extends PropertyPlaceholderConfigurer {
-
-  private static Properties localProperties;
-
-  /**
-   * 重写父类方法，解密指定属性名对应的属性值
-   */
-  @Override
-  protected String convertProperty(String propertyName, String propertyValue) {
-    if (isEncryptPropertyVal(propertyName)) {
-      return DESUtil.getDecryptString(propertyValue);//调用解密方法
-    } else {
-      return propertyValue;
-    }
-  }
-
-
-  /**
-   * 判断属性值是否需要解密
-   */
-  private boolean isEncryptPropertyVal(String propertyName) {
-    if (propertyName.contains("username") || propertyName.contains("password")) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-
-  @Override
-  protected void processProperties(
-      ConfigurableListableBeanFactory beanFactoryToProcess,
-      Properties props) throws BeansException {
-    super.processProperties(beanFactoryToProcess, props);
-    localProperties = props;
-  }
-
-  public static Properties getProperties() {
-    return localProperties;
-  }
-}
-
+//package com.chinasofti.ark.bdadp.util.common;
+//
+//import java.util.Properties;
+//import org.springframework.beans.BeansException;
+//import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+//import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+//
+///**
+// * Created by water on 2017.5.11.
+// */
+//public class DecryptPropertyPlaceholderConfigurer extends PropertyPlaceholderConfigurer {
+//
+//  private static Properties localProperties;
+//
+//  /**
+//   * 重写父类方法，解密指定属性名对应的属性值
+//   */
+//  @Override
+//  protected String convertProperty(String propertyName, String propertyValue) {
+//    if (isEncryptPropertyVal(propertyName)) {
+//      return DESUtil.getDecryptString(propertyValue);//调用解密方法
+//    } else {
+//      return propertyValue;
+//    }
+//  }
+//
+//
+//  /**
+//   * 判断属性值是否需要解密
+//   */
+//  private boolean isEncryptPropertyVal(String propertyName) {
+//    if (propertyName.contains("username") || propertyName.contains("password")) {
+//      return true;
+//    } else {
+//      return false;
+//    }
+//  }
+//
+//
+//  @Override
+//  protected void processProperties(
+//      ConfigurableListableBeanFactory beanFactoryToProcess,
+//      Properties props) throws BeansException {
+//    super.processProperties(beanFactoryToProcess, props);
+//    localProperties = props;
+//  }
+//
+//  public static Properties getProperties() {
+//    return localProperties;
+//  }
+//}
+//
