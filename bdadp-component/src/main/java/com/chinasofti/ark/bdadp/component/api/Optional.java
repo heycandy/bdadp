f635d1d63a21cb43550d546b23ae9da0e29821d1
baiyu <baiyu@chinasofti.com>
2017-06-05 14:20:56 +0800

    feat: add optional.

---
+++ bdadp-component/src/main/java/com/chinasofti/ark/bdadp/component/api/Optional.java
@@ -0,0 +1,11 @@
+package com.chinasofti.ark.bdadp.component.api;
+
+import com.chinasofti.ark.bdadp.component.api.options.ScenarioOptions;
+
+/**
+ * Created by White on 2017/6/5.
+ */
+public interface Optional {
+
+  void options(ScenarioOptions options);
+}
--- bdadp-component/src/main/java/com/chinasofti/ark/bdadp/component/support/SimpleTask.java
+++ bdadp-component/src/main/java/com/chinasofti/ark/bdadp/component/support/SimpleTask.java
@@ -3,9 +3,11 @@ package com.chinasofti.ark.bdadp.component.support;
 import com.chinasofti.ark.bdadp.component.api.CallableComponent;
 import com.chinasofti.ark.bdadp.component.api.Component;
 import com.chinasofti.ark.bdadp.component.api.Configureable;
+import com.chinasofti.ark.bdadp.component.api.Optional;
 import com.chinasofti.ark.bdadp.component.api.RunnableComponent;
 import com.chinasofti.ark.bdadp.component.api.data.Data;
 import com.chinasofti.ark.bdadp.component.api.options.ScenarioOptions;
+
 import org.slf4j.Logger;
 
 import java.io.IOException;
@@ -17,86 +19,91 @@ import java.lang.reflect.InvocationTargetException;
  */
 public class SimpleTask<K extends Component> extends AbstractTask<Data> {
 
-    protected final Class<K> _clazz;
+  protected final ScenarioOptions _options;
+  protected final Class<K> _clazz;
 
-    protected K obj = null;
+  protected K obj = null;
 
-    public SimpleTask(String id, String name, ScenarioOptions options, Class<K> clazz)
-            throws IOException {
-        super(id, name, TaskLogProvider.getLog(id, options.getExecutionId()), options);
+  public SimpleTask(String id, String name, ScenarioOptions options, Class<K> clazz)
+      throws IOException {
+    super(id, name, TaskLogProvider.getLog(id, options.getExecutionId()), options);
 
-        _clazz = clazz;
+    _options = options;
+    _clazz = clazz;
+  }
+
+  protected void init()
+      throws NoSuchMethodException, IllegalAccessException, InvocationTargetException,
+             InstantiationException {
+    Constructor<K>
+        constructor =
+        this._clazz.getConstructor(String.class, String.class, Logger.class);
+    this.obj = constructor.newInstance(getId(), getName(), getLog());
+
+  }
+
+  protected void configure() {
+    if (this.obj instanceof Configureable) {
+      ((Configureable) this.obj).configure(props);
+    }
+    if (this.obj instanceof Optional) {
+      ((Optional) this.obj).options(_options);
+    }
+  }
+
+  protected void execute() throws Exception {
+    if (this.obj instanceof RunnableComponent) {
+      ((RunnableComponent) obj).run();
+    } else if (this.obj instanceof CallableComponent) {
+      ((CallableComponent) this.obj).call();
+    } else {
+      throw new RuntimeException("TypeCheckError");
+    }
+  }
+
+  protected void exception(Exception e) {
+    throw new RuntimeException(e);
+  }
+
+  protected void finish() {
+    TaskLogProvider.close(getLog());
+  }
+
+
+  @Override
+  public void run() {
+    setState(TaskState.COMPLETING.name());
+    try {
+      info(String.format("init %s.", this));
+      this.init();
+      info(String.format("configure %s.", this));
+      this.configure();
+      info(String.format("execute %s.", this));
+      this.execute();
+
+    } catch (Exception e) {
+      setState(TaskState.EXCEPTIONAL.name());
+      error(String.format("exception %s.", this), e);
+      this.finish();
+      this.exception(e);
     }
 
-    protected void init()
-            throws NoSuchMethodException, IllegalAccessException, InvocationTargetException,
-            InstantiationException {
-        Constructor<K>
-                constructor =
-                this._clazz.getConstructor(String.class, String.class, Logger.class);
-        this.obj = constructor.newInstance(getId(), getName(), getLog());
+    setState(TaskState.NORMAL.name());
+    info(String.format("finish %s.", this));
+    this.finish();
+  }
 
-    }
+  @Override
+  public double getProgress() throws Exception {
+    return 0;
+  }
 
-    protected void configure() {
-        if (this.obj instanceof Configureable) {
-            ((Configureable) this.obj).configure(props);
-        }
-    }
+  public String getScenarioId() {
+    return this.options().as(ScenarioOptions.class).getScenarioId();
+  }
 
-    protected void execute() throws Exception {
-        if (this.obj instanceof RunnableComponent) {
-            ((RunnableComponent) obj).run();
-        } else if (this.obj instanceof CallableComponent) {
-            ((CallableComponent) this.obj).call();
-        } else {
-            throw new RuntimeException("TypeCheckError");
-        }
-    }
-
-    protected void exception(Exception e) {
-        throw new RuntimeException(e);
-    }
-
-    protected void finish() {
-        TaskLogProvider.close(getLog());
-    }
-
-
-    @Override
-    public void run() {
-        setState(TaskState.COMPLETING.name());
-        try {
-            info(String.format("init %s.", this));
-            this.init();
-            info(String.format("configure %s.", this));
-            this.configure();
-            info(String.format("execute %s.", this));
-            this.execute();
-
-        } catch (Exception e) {
-            setState(TaskState.EXCEPTIONAL.name());
-            error(String.format("exception %s.", this), e);
-            this.finish();
-            this.exception(e);
-        }
-
-        setState(TaskState.NORMAL.name());
-        info(String.format("finish %s.", this));
-        this.finish();
-    }
-
-    @Override
-    public double getProgress() throws Exception {
-        return 0;
-    }
-
-    public String getScenarioId() {
-        return this.options().as(ScenarioOptions.class).getScenarioId();
-    }
-
-    public String getExecutionId() {
-        return this.options().as(ScenarioOptions.class).getExecutionId();
-    }
+  public String getExecutionId() {
+    return this.options().as(ScenarioOptions.class).getExecutionId();
+  }
 
 }
