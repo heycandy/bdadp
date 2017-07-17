9953bd2a5e3809aa30daebdc60b36017b3954f7d
wangweiwei <672684719@qq.com>
2017-07-17 15:11:46 +0800

    update:机器学习模型保存路径先判断是否存在，如果存在则先删除

--- bdadp-util/src/main/java/com/chinasofti/ark/bdadp/util/common/FileUtils.java
+++ bdadp-util/src/main/java/com/chinasofti/ark/bdadp/util/common/FileUtils.java
@@ -1,8 +1,43 @@
 package com.chinasofti.ark.bdadp.util.common;
 
+
+import java.io.IOException;
+import org.apache.hadoop.fs.FileSystem;
+
 /**
- * Created by Administrator on 2016/9/26.
+ * Created by water on 2017.7.17.
  */
 public class FileUtils {
 
+  public static String HDFS_PATH = "hdfs://";
+
+  public static void checkDirExists(String path) {
+
+    if (path.contains(HDFS_PATH)) {
+
+      org.apache.hadoop.fs.Path hdfsPath = new org.apache.hadoop.fs.Path(path);
+      FileSystem hdfs = null;
+      try {
+        hdfs = FileSystem.get(
+            new java.net.URI(path), new org.apache.hadoop.conf.Configuration());
+        if (hdfs.exists(hdfsPath)) {
+          hdfs.delete(hdfsPath, true);
+        }
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+
+    } else {
+      java.io.File filePath = new java.io.File(path);
+      if (filePath.exists()) {
+        try {
+          org.apache.commons.io.FileUtils.deleteDirectory(filePath);
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+  }
+
 }
+
